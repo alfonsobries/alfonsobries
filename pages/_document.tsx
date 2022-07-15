382cb4f3c5a34072346bb8870537ea53491eceab
alfonsobries <alfonsobries@gmail.com>
2022-07-15 08:51:58 -0500

    image testing

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&family=Square+Peg&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="bg-white dark:bg-gray-900">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
