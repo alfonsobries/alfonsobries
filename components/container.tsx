632015cbcb9e14b2a3823b55e8a28330c5247185
alfonsobries <alfonsobries@gmail.com>
2022-07-20 19:03:51 -0500

    clenanup

--- components/container.tsx
+++ components/container.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+
 type Props = {
   children?: React.ReactNode;
   className?: string;
@@ -5,7 +7,7 @@ type Props = {
 
 const Container = ({ children, className }: Props) => {
   return (
-    <div className={`max-w-xl mx-auto px-5${className ? ` ${className}` : ``}`}>
+    <div className={classNames("max-w-xl mx-auto px-4", className)}>
       {children}
     </div>
   );
