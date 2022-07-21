792f1fd5c631c2a45620f57267fa09db24591987
alfonsobries <alfonsobries@gmail.com>
2022-07-21 16:45:44 -0500

    Remove use small

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -15,34 +15,16 @@ const PageHeader = () => {
       setTheme("dark");
     }
   };
-  const useSmall = false;
 
   return (
     <div className="z-50 mb-4">
       <Container noPadding>
-        <div
-          className={classNames(BORDER_COLOR, "px-4", {
-            "py-4": !useSmall,
-            "py-3": useSmall,
-          })}
-        >
-          <div
-            className={classNames(
-              "relative flex sm:flex-row  sm:justify-center sm:border-b",
-              {
-                "flex-col items-center space-y-4 sm:flex-row sm:items-end sm:space-y-0 sm:space-x-8  ":
-                  !useSmall,
-                "flex-row-reverse items-end justify-between": useSmall,
-              }
-            )}
-          >
+        <div className={classNames(BORDER_COLOR, "px-4 py-4")}>
+          <div className="relative flex flex-col  items-center space-y-4 sm:flex-row sm:items-end sm:justify-center sm:space-y-0 sm:space-x-8 sm:border-b">
             <button
               type="button"
               onClick={toggleDarkMode}
-              className={classNames({
-                "absolute right-0 top-0 -mt-2 w-12 p-1": !useSmall,
-                "w-6": useSmall,
-              })}
+              className="absolute right-0 top-0 -mt-2 w-10 p-1"
             >
               <img
                 src={
@@ -68,16 +50,14 @@ const PageHeader = () => {
               />
             </div>
 
-            {!useSmall && (
-              <div className="w-full sm:w-auto sm:flex-grow sm:space-y-4">
-                <p className="text-center font-cursive text-6xl font-bold text-gray-900 dark:text-gray-300 sm:text-left">
-                  Hello, I’m{" "}
-                  <span className="relative after:absolute after:left-0 after:-mt-[15px] after:-ml-[5%] after:block after:h-3 after:w-[110%] after:bg-[#fbd68b] after:opacity-50 after:content-['']">
-                    Alfonso
-                  </span>
-                </p>
-              </div>
-            )}
+            <div className="w-full sm:w-auto sm:flex-grow sm:space-y-4">
+              <p className="text-center font-cursive text-6xl font-bold text-gray-900 dark:text-gray-300 sm:text-left">
+                Hello, I’m{" "}
+                <span className="relative after:absolute after:left-0 after:-mt-[15px] after:-ml-[5%] after:block after:h-3 after:w-[110%] after:bg-[#fbd68b] after:opacity-50 after:content-['']">
+                  Alfonso
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </Container>
