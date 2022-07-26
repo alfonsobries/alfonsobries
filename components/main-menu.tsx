f63b1d243e853ab563b346ae18ccaa5c02552a14
alfonsobries <alfonsobries@gmail.com>
2022-07-25 22:56:20 -0500

    main menu wip

--- components/main-menu.tsx
+++ components/main-menu.tsx
@@ -2,10 +2,29 @@ import Link from "next/link";
 import cn from "classnames";
 import { useRouter } from "next/router";
 import { BORDER_COLOR } from "../lib/cssClasses";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Container from "./container";
 
 const links = [
+  {
+    href: "/",
+    label: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg>
+    ),
+  },
   {
     href: "/about",
     label: "About",
@@ -60,11 +79,23 @@ const MainMenu = () => {
     setMenuOpened(!menuOpened);
   }, [menuOpened]);
 
+  const useDropdownMenu = useMemo(() => isSticky, [isSticky]);
+
+  const showDropdownMenu = useMemo(
+    () => useDropdownMenu && menuOpened,
+    [menuOpened, useDropdownMenu]
+  );
+
   return (
     <nav
       className={cn(
         BORDER_COLOR,
-        "no-scrollbar sticky top-0 overflow-auto border-b bg-white/30 backdrop-blur-lg dark:bg-gray-900/30"
+        "no-scrollbar top-0 mb-8 overflow-auto border-b",
+        {
+          "fixed h-screen w-screen bg-white ": showDropdownMenu,
+          "sticky bg-white/30 backdrop-blur-lg dark:bg-gray-900/30":
+            !showDropdownMenu,
+        }
       )}
     >
       <Container
@@ -127,55 +158,29 @@ const MainMenu = () => {
           "mx-auto mb-[-1px] flex max-w-xl justify-between justify-items-stretch px-4",
           {
             hidden: isSticky && !menuOpened,
+            "flex-col": useDropdownMenu,
           }
         )}
       >
-        <li>
-          <Link href="/">
-            <a
-              className={cn(
-                "block h-full whitespace-nowrap px-2 py-3 text-blue-700 dark:text-blue-200",
-                {
-                  "border-b-2 border-blue-600 dark:border-blue-500":
-                    router.pathname === "/",
-                  "hover:text-blue-600 hover:underline":
-                    router.pathname !== "/",
-                }
-              )}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
-            </a>
-          </Link>
-        </li>
-
         {links.map(({ href, label }, index) => (
           <li key={href}>
             <Link href={href}>
               <a
                 className={cn(
-                  "block h-full whitespace-nowrap px-2 py-3 text-blue-700 dark:text-blue-200",
+                  "block h-full whitespace-nowrap py-3 text-blue-700 dark:text-blue-200",
                   {
                     "border-b-2 border-blue-600 dark:border-blue-500":
-                      router.pathname === href,
+                      router.pathname === href && !useDropdownMenu,
                     "hover:text-blue-600 hover:underline":
-                      router.pathname !== href,
+                      router.pathname !== href && !useDropdownMenu,
+                    "px-2": !useDropdownMenu,
+                    "border-t": useDropdownMenu,
+                    "font-semibold":
+                      useDropdownMenu && router.pathname === href,
                   }
                 )}
               >
-                {label}
+                {index === 0 && menuOpened ? "Home" : label}
               </a>
             </Link>
           </li>
