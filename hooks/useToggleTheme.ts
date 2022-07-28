28c25a656045c541994b9c3f839d760cb074c7b0
alfonsobries <alfonsobries@gmail.com>
2022-07-27 20:11:06 -0500

    fix theme

--- hooks/useToggleTheme.ts
+++ hooks/useToggleTheme.ts
@@ -1,19 +1,25 @@
 import { useTheme } from "next-themes";
+import { useCallback, useEffect, useState } from "react";
 
 const useToggleTheme = () => {
   const { theme, setTheme } = useTheme();
+  const [realTheme, setRealTheme] = useState<string>();
 
-  const toggleTheme = () => {
+  const toggleTheme = useCallback(() => {
     if (theme === "dark") {
       setTheme("light");
     } else {
       setTheme("dark");
     }
-  };
+  }, [theme, setTheme]);
+
+  useEffect(() => {
+    setRealTheme(theme);
+  }, [theme, setRealTheme]);
 
   return {
     toggleTheme,
-    theme,
+    theme: realTheme,
   };
 };
 
