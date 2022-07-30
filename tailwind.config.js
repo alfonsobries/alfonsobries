4235fb2fc29a63b73e64d3986100e3dc6cd8b7dd
alfonsobries <alfonsobries@gmail.com>
2022-07-30 13:29:47 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./lib/cssClasses.ts",
+    "./public/images/**/*.svg",
   ],
   theme: {
     extend: {
