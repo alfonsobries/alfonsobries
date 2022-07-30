524270cf4a7dcb1fa5ccbaf94511741118a7e443
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-07-29 19:47:38 -0500

    Add svgs to purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/images/**/*.svg",
     "./lib/cssClasses.ts",
   ],
   theme: {
