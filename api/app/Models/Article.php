9af7d09342ced5bd977ac7868d5b3deb2b2af288
alfonsobries <alfonsobries@gmail.com>
2022-08-02 11:15:00 -0500

    Add article and article managment

---
+++ api/app/Models/Article.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+
+class Article extends Model implements HasMedia
+{
+    use HasFactory;
+    use HasSlug;
+    use InteractsWithMedia;
+    use SoftDeletes;
+
+    protected $casts = [
+        'published_at'      => 'datetime',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'title',
+        'body',
+        'published_at',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    public function scopePublished(Builder $query): Builder
+    {
+        return $query->where('is_published', true);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('banner')->singleFile();
+    }
+}
---
+++ api/app/Nova/Article.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova;
+
+use App\Models\Article as Model;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Markdown;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Resource;
+use Laravel\Nova\Fields\Image;
+
+final class Article extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = Model::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'title', 'body',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        /** @var Model */
+        $resource = $this->resource;
+
+        return [
+            ID::make()->sortable(),
+
+            Text::make('Title', 'title')
+                ->sortable()
+                ->rules('required', 'string', 'max:120'),
+
+            Textarea::make('Meta description', 'meta_description')
+                ->rules('nullable', 'string', 'max:155')
+                ->hideFromIndex(),
+
+            Markdown::make('Body')
+                ->rules('required', 'string')
+                ->hideFromIndex(),
+
+            Image::make('Banner', 'banner')
+                ->rules('image', 'max:2048')
+                ->store(function ($request, $model) {
+                    $model
+                        ->addMedia($request->file('banner'))
+                        ->toMediaCollection('banner');
+
+                    return [];
+                })
+                ->deletable(false)
+                ->thumbnail(function () use ($resource) {
+                    /** @var Article $resource */
+                    return $resource->getFirstMediaUrl('banner');
+                })
+                ->preview(function () use ($resource) {
+                    info($resource->getFirstMediaUrl('banner'));
+                    /** @var Article $resource */
+                    return $resource->getFirstMediaUrl('banner');
+                })
+                ->disableDownload(),
+
+            DateTime::make('Published at')
+                ->sortable()
+                ->nullable()
+                ->help('Set a published date to publish the article'),
+        ];
+    }
+
+    protected static function afterValidation(NovaRequest $request, $validator) : void
+    {
+        /** @var Model|null $model */
+        $model = $request->findModel();
+
+        if ($model?->getFirstMedia() !== null) {
+            return;
+        }
+
+        if ($request->input('published_at') === null) {
+            return;
+        }
+
+        if ($request->has('banner')) {
+            return;
+        }
+
+        $validator->errors()->add('banner', 'The banner is required when the article is set to be published');
+    }
+}
