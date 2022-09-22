655c83c32ea52c79d3ebba906b1c5e9be3ddd4b8
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-09-22 12:43:00 -0500

    bg

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,7 +9,7 @@ export default function Error404() {
       <Head>
         <title>@TODO 404 | {CMS_NAME}</title>
       </Head>
-      <div className="absolute inset-0 flex h-screen w-full overflow-hidden">
+      <div className="absolute inset-0 flex h-screen w-full overflow-hidden bg-[#BFD57B]">
         <LazySvg
           className="absolute right-0 top-0 w-full"
           svgClassName="object-cover"
@@ -21,9 +21,13 @@ export default function Error404() {
           src="/images/animations/this-is-fine.svg"
         />
         <LazySvg
+          className="absolute left-0 bottom-0 -mr-[140px] w-[500px] max-w-full"
+          src="/images/animations/right-flame-loop.svg"
+        />
+        {/* <LazySvg
           className="absolute left-0 bottom-0 -ml-[140px] -mb-[100px] w-[700px] max-w-full"
           src="/images/animations/this-is-fine.svg"
-        />
+        /> */}
       </div>
     </>
   );
