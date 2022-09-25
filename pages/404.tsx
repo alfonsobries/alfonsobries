239bf15894490db5676d6e574ba958d4443460ee
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-09-25 16:54:43 -0500

    Better cloud animation

--- pages/404.tsx
+++ pages/404.tsx
@@ -11,14 +11,9 @@ export default function Error404() {
       </Head>
       <div className="absolute inset-0 flex h-screen w-full items-center justify-center overflow-hidden bg-[#BFD57B]">
         <LazySvg
-          className="absolute right-0 top-0 w-full"
+          className="absolute right-0 top-0 z-50 w-full"
           svgClassName="object-cover"
           src="/images/animations/cloud.svg"
-        />
-
-        <LazySvg
-          className="absolute right-0 top-[200px] z-10 w-[300px] max-w-full"
-          src="/images/animations/flame-bottom-right.svg"
         />
 
         <LazySvg
@@ -31,6 +26,13 @@ export default function Error404() {
         /> */}
 
         <div className="absolute bottom-0 left-0 z-0 h-[50%] w-full border-2 border-black bg-[#AD8665]"></div>
+
+        {/* <div className="absolute z-50 mt-[-350px] ml-[200px] flex flex-col rounded-xl border-2 border-black bg-white p-6 text-center">
+          <span className=" font-cursive text-5xl text-gray-900">
+            404 Page not found
+            <br /> THIS IS FINE
+          </span>
+        </div> */}
 
         <LazySvg
           className="absolute right-0 bottom-0 z-20 w-[500px] max-w-full"
