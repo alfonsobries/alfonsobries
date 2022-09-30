bb7ad8aa286717ba5b53232cdf5669046c48d8f7
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-09-30 17:16:16 -0500

    flame top right

--- pages/404.tsx
+++ pages/404.tsx
@@ -17,18 +17,23 @@ export default function Error404() {
         />
 
         <LazySvg
-          className="relative z-10 h-auto w-[600px]"
+          className="absolute right-0 bottom-[40%] z-20 w-[600px] max-w-full"
+          src="/images/flame-top-right.svg"
+        />
+
+        <LazySvg
+          className="relative z-20 h-auto w-[600px]"
           src="/images/this-is-fine.svg"
         />
 
         <div className="absolute bottom-0 left-0 z-0 h-[50%] w-full border-2 border-black bg-[#AD8665]"></div>
 
         <LazySvg
-          className="absolute right-0 bottom-0 z-20 w-[500px] max-w-full"
+          className="absolute right-0 bottom-0 z-30 w-[500px] max-w-full"
           src="/images/flame-bottom-right.svg"
         />
         <LazySvg
-          className="absolute left-0 bottom-0 z-20 w-[400px] max-w-full"
+          className="absolute left-0 bottom-0 z-30 w-[400px] max-w-full"
           src="/images/flame-bottom-left.svg"
         />
       </div>
---
+++ pages/resume.tsx
@@ -0,0 +1,76 @@
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { getAllPosts } from "../lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import Post from "../interfaces/post";
+import ArticleListItem from "../components/article-list-item";
+import { useEffect } from "react";
+
+type Props = {
+  allPosts: Post[];
+};
+
+export default function Index({ allPosts }: Props) {
+  const posts = allPosts.slice(0, 3);
+
+  return (
+    <>
+      <Layout
+        meta={{
+          title: "Tech, Development, and more",
+          description: "@TODO: TBD",
+          image: `https://og.alfonsobries.com/@TODO.png`,
+          // path: ``,
+          // ogType: "article",
+        }}
+      >
+        <Container>
+          <div className="prose prose-h2:text-lg dark:prose-invert">
+            <h1>Latest Posts</h1>
+
+            <div>
+              {posts.map((post) => (
+                <ArticleListItem key={post.slug} post={post} />
+              ))}
+              {posts.map((post) => (
+                <ArticleListItem key={post.slug} post={post} />
+              ))}
+            </div>
+          </div>
+
+          {/* <Intro /> */}
+          {/* {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const allPosts = await getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  return {
+    props: { allPosts },
+  };
+};
