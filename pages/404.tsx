440dac2ad6503d90ad1e7ec13a56398ac7655e57
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-10-03 22:23:49 -0500

    error 404 test

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,11 +10,12 @@ export default function Error404() {
         <title>Error 404 - Page not Found! | {CMS_NAME}</title>
       </Head>
       <div className="absolute inset-0 flex h-full w-full items-center justify-center overflow-hidden bg-[#BFD57B]">
-        {/* <div className="absolute top-0 left-0 z-50 flex w-full justify-center ">
-          <span className="rounded-b-lg border-2 border-t-0 border-black bg-white py-5 pt-40 text-5xl font-bold leading-none text-black">
-            Error 404
-          </span>
-        </div> */}
+        <LazySvg
+          className="absolute right-0 top-0 z-30 w-full"
+          svgClassName="object-cover"
+          src="/images/cloud-2.svg"
+        />
+
         <LazySvg
           className="absolute right-0 top-0 z-50 w-full"
           svgClassName="object-cover"
@@ -31,20 +32,28 @@ export default function Error404() {
           src="/images/flame-top-right.svg"
         />
 
-        <LazySvg
-          className="relative z-20 h-auto w-[600px]"
-          src="/images/this-is-fine.svg"
-        />
+        <div className="relative z-40 h-auto w-[600px]">
+          <div className="flex-0 absolute inset-0 z-50 mt-[-30px] ml-[200px] aspect-video w-[300px] bg-[url('/images/globe.svg')] bg-contain bg-center bg-no-repeat">
+            <div className="absolute inset-0 mb-[10%] mt-[3%] flex items-center justify-center overflow-hidden">
+              <span className="text-center  text-black">
+                <span className="text-4xl font-bold">Error 404</span> <br />{" "}
+                <span className="text-3xl font-semibold">Not Found</span>
+              </span>
+            </div>
+          </div>
+
+          <LazySvg className="" src="/images/this-is-fine.svg" />
+        </div>
 
         <div className="absolute bottom-0 left-0 z-0 h-[50%] w-full border-2 border-black bg-[#AD8665]"></div>
 
         <LazySvg
-          className="absolute left-[70%] bottom-0 z-30 max-h-[60%] w-[500px] max-w-full md:left-auto md:right-0"
+          className="absolute left-[70%] bottom-0 z-40 max-h-[60%] w-[500px] max-w-full md:left-auto md:right-0"
           src="/images/flame-bottom-right.svg"
         />
 
         <LazySvg
-          className="absolute left-[-30%] bottom-[-30%] z-30 max-h-[70%] w-[400px] max-w-full sm:bottom-0 md:left-0"
+          className="absolute left-[-30%] bottom-[-30%] z-40 max-h-[70%] w-[400px] max-w-full sm:bottom-0 md:left-0"
           src="/images/flame-bottom-left.svg"
         />
       </div>
