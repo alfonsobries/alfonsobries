44d5c4e831b8394cd3b5cc339080bc3ddb0d7b61
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-10-06 21:39:57 -0500

    Update switch-button.tsx

--- components/switch-button.tsx
+++ components/switch-button.tsx
@@ -64,6 +64,10 @@ const SwitchButton: React.FC<{
           d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
         />
       </svg>
+
+      <span className="sr-only">
+        {resolvedTheme === "dark" ? "Use Light Mode" : "Use Dark Mode"}
+      </span>
     </button>
   );
 };
