571f8bc60beae7617493562d577f1390157ea659
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-10-11 16:09:18 -0500

    cleanup

--- api/app/Http/Controllers/ContactFormController.php
+++ api/app/Http/Controllers/ContactFormController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\ContactFormEmail;
-use App\Models\ContactFormMessage;
 use App\Models\User;
 use App\Notifications\ContactFormNotification;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 
 class ContactFormController extends Controller
 {
