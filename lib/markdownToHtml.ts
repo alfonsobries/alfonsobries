a0cdf39adb0ea7fe714f715206ccf7ec5deabca3
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-10-11 17:17:58 -0500

    remark consider make links external

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,9 +2,11 @@ import { remark } from "remark";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
+import remarkExternalLinks from "remark-external-links";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
+    .use(remarkExternalLinks, { target: "_blank", rel: ["nofollow"] })
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: true }) // Pass raw HTML strings through.
     .use(rehypeStringify, { allowDangerousHtml: true }) // Serialize the raw HTML strings
