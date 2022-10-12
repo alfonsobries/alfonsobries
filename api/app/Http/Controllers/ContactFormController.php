73f540e862155db41580ffd7b1fb8944da128847
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-10-12 15:55:31 -0500

    Accept typo messages

--- api/app/Http/Controllers/ContactFormController.php
+++ api/app/Http/Controllers/ContactFormController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use App\Models\User;
 use App\Notifications\ContactFormNotification;
+use App\Notifications\TypoNotification;
 use Illuminate\Http\Request;
 
 class ContactFormController extends Controller
@@ -11,12 +13,27 @@ class ContactFormController extends Controller
     public function __invoke(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:254',
+            'typo' => 'nullable|boolean',
+            'name' => 'required_unless:typo,true|max:255',
+            'post_slug' => 'required_if:typo,true|max:255',
+            'email' => 'required_unless:typo,true|email|max:254',
             'message' => 'required|max:1000',
+            'typo_excerpt' => 'nullable|string',
         ]);
 
-        User::whereEmail('alfonso@vexilo.com')->first()->notify(new ContactFormNotification(
+        $notifable = User::whereEmail('alfonso@vexilo.com')->first();
+
+        if ($request->boolean('typo')) {
+            $notifable->notify(new TypoNotification(
+                article: Article::whereSlug($request->get('post_slug'))->first(),
+                message: $request->get('message'),
+                excerpt: $request->get('typo_excerpt'),
+            ));
+
+            return;
+        }
+
+        $notifable->notify(new ContactFormNotification(
             name: $request->get('name'),
             email: $request->get('email'),
             message: $request->get('message'),
---
+++ api/app/Notifications/TypoNotification.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Article;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use NotificationChannels\Telegram\TelegramMessage;
+
+class TypoNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(private Article $article, private string $message, private ?string $excerpt = null)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'database', 'telegram'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $message = (new MailMessage)
+                    ->subject($this->subject())
+                    ->greeting($this->subject())
+                    ->line($this->getHtmlableLine('<strong>Article Title:</strong> ' . $this->article->title))
+                    ->line($this->getHtmlableLine('<strong>Article Slug:</strong> ' . $this->article->slug))
+                    ->line($this->getHtmlableLine('<strong>Message:</strong>'))
+                    ->line($this->getHtmlableLine(nl2br($this->message)));
+
+        if ($this->excerpt) {
+            $message->line($this->getHtmlableLine('<strong>Message:</strong>'))
+                ->line($this->getHtmlableLine(nl2br($this->message)));
+        }
+
+        return $message->action('Edit Article', $this->editUrl())
+                    ->line($this->getHtmlableLine(sprintf('<p style="text-align: center"><a href="%s">View Article</a></p>', $this->viewUrl())));
+    }
+
+    public function subject(): string
+    {
+        return sprintf('Typo on article: %s (%s)', $this->article->title, $this->article->slug);
+    }
+
+    public function editUrl(): string
+    {
+        return url(sprintf('nova/resources/articles/%s/edit', $this->article->id));
+    }
+    public function viewUrl(): string
+    {
+        return sprintf('%s/posts/%s', config('site.site_url'), $this->article->slug);
+    }
+
+    public function getHtmlableLine($html): Htmlable
+    {
+        return new class($html) implements Htmlable {
+            public function __construct(private string $html)
+            {
+                //
+            }
+
+            public function toHtml()
+            {
+                return $this->html;
+            }
+        };
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'type' => 'contact-form',
+            'data' => [
+                'excerpt' => $this->excerpt,
+                'articleSlug' => $this->article->slug,
+                'message' => $this->message,
+            ],
+        ];
+    }
+
+
+    public function toTelegram($notifiable)
+    {
+        $markdown = <<<MARKDOWN
+*%s*
+
+*Message:*
+%s
+
+*Excerpt:*
+%s
+MARKDOWN;
+
+        return TelegramMessage::create()
+            ->to(config('services.telegram-bot-api.chat_id'))
+            ->content(sprintf($markdown, $this->subject(),  $this->message, $this->excerpt ?? '**No excerpt provided**'))
+            ->button('View Article', $this->viewUrl())
+            ->button('Edit Article', $this->editUrl());
+    }
+}
