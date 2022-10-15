a1a152b8e6159f0d8a67353d2b0322d20e78c30b
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-10-15 16:20:43 -0500

    use preview and view labels

--- api/app/Nova/Article.php
+++ api/app/Nova/Article.php
@@ -52,12 +52,10 @@ final class Article extends Resource
 
             Text::make('URL', function ($model) {
                 if ($model->isPublished()) {
-                    $url = $model->url();
+                    return sprintf('<a class="link-default" target="_blank" href="%s">View</a>', $model->url());
                 } else {
-                    $url = $model->previewUrl();
+                    return sprintf('<a class="link-default" target="_blank" href="%s">Preview</a>', $model->previewUrl());
                 }
-
-                return sprintf('<a class="link-default" target="_blank" href="%s">Preview</a>', $url);
             })->asHtml(),
 
             Text::make('Title', 'title')
