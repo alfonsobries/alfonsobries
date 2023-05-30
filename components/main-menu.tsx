499c3329260b7a723e133649277b6ed3fa8f0855
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-05-30 10:47:28 -0600

    about only on dropdown menu

--- components/main-menu.tsx
+++ components/main-menu.tsx
@@ -26,6 +26,7 @@ type MenuLink = {
     | string
     | React.ReactNode
     | (({ locale }: { locale: LocaleCode }) => React.ReactNode);
+  mobileOnly?: boolean;
 };
 
 const links: MenuLink[] = [
@@ -48,9 +49,14 @@ const links: MenuLink[] = [
   },
   {
     selected: (router) => false,
-    // @TODO
     href: ({ locale }) => urls.labs({ locale }),
     label: "Labs",
+  },
+  {
+    selected: (router) => router.pathname.startsWith("/[about]"),
+    href: ({ locale }) => urls.about({ locale }),
+    label: "About",
+    mobileOnly: true,
   },
   {
     selected: (router) => false,
@@ -259,7 +265,12 @@ const MainMenu = ({
           })}
         >
           {links.map((link, index) => (
-            <li key={index}>
+            <li
+              key={index}
+              className={classNames({
+                hidden: link.mobileOnly && !menuOpened,
+              })}
+            >
               {renderMenuLink({
                 link,
                 index,
@@ -278,14 +289,30 @@ const MainMenu = ({
                   lang={router.locale}
                   hrefLang={router.locale === "en" ? "es" : "en"}
                   className={cn(
-                    "flex h-full items-center whitespace-nowrap text-blue-700 dark:text-blue-200",
+                    "flex h-full items-center space-x-1 whitespace-nowrap text-blue-700 dark:text-blue-200",
                     {
                       "px-2": !useDropdownMenu,
                       [`${BORDER_COLOR} border-t py-3`]: useDropdownMenu,
                     }
                   )}
                 >
-                  {router.locale === "en" ? "ES" : "EN"}
+                  <span className="text-xs">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+                      />
+                    </svg>
+                  </span>
+                  <span>{router.locale === "en" ? "ES" : "EN"}</span>
                 </a>
               </Link>
             </li>
