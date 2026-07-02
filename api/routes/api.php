b981e0b1fd73c17df5f87b292e5f8ba5823f35f5
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-07-02 14:01:57 -0600

    refactor: name user route and drop dead order routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -15,12 +15,7 @@ Route::get('/status', StatusController::class)->name('status');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::controller(ArticleController::class)->group(function () {
-    Route::get('/orders/{id}', 'show');
-    Route::post('/orders', 'store');
-});
+})->name('user');
 
 Route::name('articles.')
     ->prefix('/articles')
